b7b8cd8f9e8a91ddd84e21961b8d0ecbc0b1c758
Krzysztof Hewelt <krzysiu-nv@wp.pl>
2024-05-07 22:34:57 +0200

    Some fun with routing

--- web/routes/api.php
+++ web/routes/api.php
@@ -1,10 +1,24 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ImageController;
 use Illuminate\Support\Facades\Route;
 
 Route::controller(AuthController::class)->group(function () {
     Route::post('login', 'login');
+    Route::post('register', 'register');
     Route::post('logout', 'logout');
     Route::post('refresh', 'refresh');
+});
+
+Route::middleware(['auth:api'])->group(function () {
+    Route::controller(ImageController::class)->prefix('images')->group(function () {
+        Route::post('translate', 'translate');
+        Route::post('translate', 'add');
+        Route::get('all', 'all');
+        Route::get('{id}/edit', 'edit');
+        Route::patch('{id}/edit', 'edit');
+        Route::delete('{id}/delete', 'delete');
+        Route::delete('{id}/publish', 'publish');
+    });
 });
